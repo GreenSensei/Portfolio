4f6e6c186282f5f653bfadcba87cee5a2f9a58ab
Yahye Mohamed <74530335+GreenSensei@users.noreply.github.com>
2022-02-26 22:27:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="script" href="js/main.js">
+    <title>Portfolio</title>
+</head>
+<body>
+    <section id="section-title">
+        <div id="title" class="center noselect">
+            <h1 class="ikben">Hallo! Mijn naam is</h1>
+            <h1 class="yahye" data-text="Yahye Mohamed">Yahye Mohamed</h1>
+            <a href="#section-aboutMe" class="text-color">Mijn informatie</a>
+            <a href="#section-experience" class="text-color">Mijn ervaringen</a>
+        </div>
+    </section>
+
+    <section id="section-aboutMe">
+        <div id="aboutMe" class="center">
+            <h2>Welkom op mijn portfolio!</h2>
+            <p>Mijn naam is Yahye Mohamed zoals je hierboven had kunnen lezen. Ik ben een software developer en ik volg de software developer opleiding. Ik hou van het ontwikkelen van software. Het fijnste gevoel krijg ik wanneer ik het product werkend zie!</p>
+        </div>
+    </section>
+
+    <section id="section-experience">
+        <h2 class="title">Mijn ervaringen</h2>
+        <div class="container center noselect higher">
+            <div class="card">
+                <header style="background-color:#8993be">
+                    <img class="logo" src="images/php.png"
+                    <h1>PHP</h1>
+                </header>
+                <div class="content">
+                    <h2>PHP</h2>
+                    <h3>PHP ervaringen</h3>
+                    <p>Ik heb ervaring met PHP. <br>De eerste keer dat ik in aanraking kwam met deze prachtige taal was via school!</p>
+                    <a href="#">PHP werk</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <header style="background-color:#ee652a">
+                    <img class="logo" src="images/html.png"
+                    <h1>HTML</h1>
+                </header>
+                <div class="content">
+                    <h2>HTML</h2>
+                    <h3>HTML ervaringen</h3>
+                    <p>Ik heb mijn ervaring met HTML opgedaan via school en eigen projecten! Tijdens mijn stage heb ik ook geleerd te werken met Bootstrap.</p>
+                    <a href="#">HTML werk</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <header style="background-color:#1c87c6">
+                    <img class="logo" src="images/css.png"
+                    <h1>CSS</h1>
+                </header>
+                <div class="content">
+                    <h2>CSS</h2>
+                    <h3>CSS ervaringen</h3>
+                    <p>Ik heb ook mijn begin stappen met CSS geleerd op school. Dit heeft mij geholpen om dieper te duiken in het meer genaamd CSS!</p>
+                    <a href="#">CSS werk</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <header style="background-color:#e9d31c">
+                    <img class="logo" src="images/js.png"
+                    <h1>JavaScript</h1>
+                </header>
+                <div class="content">
+                    <h2>JS</h2>
+                    <h3>JS ervaringen</h3>
+                    <p>Ik heb niet zoveel ervaring met JavaScript buiten mijn stage om! Ik heb op mijn stage live charts gemaakt en heb hiervoor veel JS gebruikt.</p>
+                    <a href="#">JS werk</a>
+                </div>
+            </div>
+
+            <div class="card">
+                <header style="background-color:#232323">
+                    <img class="logo" src="images/github.png"
+                    <h1>Github</h1>
+                </header>
+                <div class="content">
+                    <h2>GHUB</h2>
+                    <h3>Mijn Github</h3>
+                    <p>Op het begin deed ik niet zoveel met Github maar na mijn stage periode gebruikte ik het steeds vaker!</p>
+                    <a href="https://github.com/GreenSensei?tab=repositories">My Github</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.0/vanilla-tilt.min.js"></script>
+</body>
+<script src="js/main.js"></script>
+</html>
